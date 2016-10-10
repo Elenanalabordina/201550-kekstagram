199e2dfa955c69ae5929588ee93ba75463ead8b8
“Elena <elenanalabordina@mail.ru>
2016-10-10 20:33:57 +0700

    +

---
+++ src/js/check.js
@@ -0,0 +1,22 @@
+
+var getMessage =function (a,b) {
+  var  a = boolean;
+  var animatedImage= true;
+  var unanimatedImage=false;
+  if(animatedImage) {
+    return("Переданное GIF-изображение анимировано и содержит [b] кадров");
+  }
+  else(unanimatedImage){
+    return("Переданное GIF-изображение не анимировано");
+  }
+  if(a=number) {
+    return("Переданное SVG-изображение содержит [a] объектов и [b * 4] атрибутов";)
+  }
+  var amountOfRedPoints = [+]
+  if Array.isArray([a]) {
+    return("Количество красных точек во всех строчках изображения: [amountOfRedPoints]")
+  }
+  var artifactsSquare = [a+b]
+} if Array.isArray([a],[b]) {
+  return("Общая площадь артефактов сжатия: [artifactsSquare] пикселей")
+}
