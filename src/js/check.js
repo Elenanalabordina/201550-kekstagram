ee602371dadcc5cd682aa52f66a3d257cb9dce96
“Elena <elenanalabordina@mail.ru>
2016-10-11 20:09:25 +0700

    Поправила по замечаниям

---
+++ src/js/check.js
@@ -0,0 +1,23 @@
+
+var getMessage =function (a,b) {
+  if(a===true) {
+    return("Переданное GIF-изображение анимировано и содержит [b] кадров");
+  }
+  else(a===false){
+    return("Переданное GIF-изображение не анимировано");
+  }
+  if(a===number) {
+    return("Переданное SVG-изображение содержит [a] объектов и [b * 4] атрибутов";)
+  }
+  var amountOfRedPoints = [+]
+  if Array.isArray(a) {
+    return("Количество красных точек во всех строчках изображения: [amountOfRedPoints]")
+  }
+  if Array.isArray(a),
+   Array.isArray(b) {
+  return("Общая площадь артефактов сжатия: [artifactsSquare] пикселей")
+  }
+  var a = [];
+  var b = [];
+  var artifactsSquare = (a[1]*b[1]+a[2]*b[2])
+}
