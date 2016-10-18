b48dc7e85aaa6117b7edc29a424a2e10f188c2e7
“Elena <elenanalabordina@mail.ru>
2016-10-18 22:16:29 +0700

    последнее+

--- src/js/check.js
+++ src/js/check.js
@@ -1,26 +1,33 @@
 
-var getMessage = function (a,b) {
+  var getMessage =function (a,b) {
   if(a===true) {
-    return("Переданное GIF-изображение анимировано и содержит [b] кадров");
+    return "Переданное GIF-изображение анимировано и содержит"+ b +"кадров";
   }
-  else(a===false){
-    return("Переданное GIF-изображение не анимировано");
+   {
+    return "Переданное GIF-изображение не анимировано";
   }
-  if(a===number) {
-    return("Переданное SVG-изображение содержит [a] объектов и [b * 4] атрибутов";)
+  if(typeof a==="number") {
+    return "Переданное SVG-изображение содержит" + a + "объектов и" + b * 4 + "атрибутов";
   }
-  var amountOfRedPoints = [];
-  var sumRedPoints = 0;
-  for (var i=0; i<=amountOfRedPoints.length-1; i++) {
-    sumRedPoints += amountOfRedPoints[i];
+  if(Array.isArray(a)) {
+   var amountOfRedPoints = 0;
+   for(var i = 0; i < a.length; i++){
+     amountOfRedPoints += a[i];
+   }
+     return "Количество красных точек во всех строчках изображения:" + amountOfRedPoints;
+   }
+
+   if (Array.isArray(a) && Array.isArray(b)) {
+    var artifactsSquare = 0
+    var sumA = 0;
+    var sumB = 0;
+    for (var i=0; i<a.length; i++) {
+      sumA += a[i];
+      sumB += b[i]
+    artifactsSquare += sumA  * sumB;
+   }
+   return "Общая площадь артефактов сжатия:"+ artifactsSquare +"пикселей";
+ }
+ {
+    return "Переданы некорректные данные";
   }
-  if (Array.isArray(a)) {
-    return("Количество красных точек во всех строчках изображения: [amountOfRedPoints]")
-  }
-  if (Array.isArray(a)&&Array.isArray(b)) {
-  return("Общая площадь артефактов сжатия: [artifactsSquare] пикселей")
-  }
-  var a = [];
-  var b = [];
-  var artifactsSquare = (a[1]*b[1]+a[2]*b[2])
-}
