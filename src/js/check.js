3de2f9fdaedf0bcc0a0ca211e937ba137c37c407
“Elena <elenanalabordina@mail.ru>
2016-11-05 18:36:54 +0700

    check.js

--- src/js/check.js
+++ src/js/check.js
@@ -1,38 +1,6 @@
 
   var getMessage =function (a,b) {
   if(a===true) {
-<<<<<<< HEAD
-    return "Переданное GIF-изображение анимировано и содержит"+ b +"кадров";
-  }
-   {
-    return "Переданное GIF-изображение не анимировано";
-  }
-  if(typeof a==="number") {
-    return "Переданное SVG-изображение содержит" + a + "объектов и" + b * 4 + "атрибутов";
-  }
-  if(Array.isArray(a)) {
-   var amountOfRedPoints = 0;
-   for(var i = 0; i < a.length; i++){
-     amountOfRedPoints += a[i];
-   }
-     return "Количество красных точек во всех строчках изображения:" + amountOfRedPoints;
-   }
-
-   if (Array.isArray(a) && Array.isArray(b)) {
-    var artifactsSquare = 0
-    var sumA = 0;
-    var sumB = 0;
-    for (var i=0; i<a.length; i++) {
-      sumA += a[i];
-      sumB += b[i]
-    artifactsSquare += sumA  * sumB;
-   }
-   return "Общая площадь артефактов сжатия:"+ artifactsSquare +"пикселей";
- }
- {
-    return "Переданы некорректные данные";
-  }
-=======
     return "Переданное GIF-изображение анимировано и содержит " + b + " кадров";
   }
   if (a === false) {
@@ -64,4 +32,3 @@
  }
     return "Переданы некорректные данные";
   };
->>>>>>> 48231102b65a306bc6a3279aba7394def7e8db8d
